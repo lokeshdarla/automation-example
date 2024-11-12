4599408849a9a70641f7574a017b49a845ed18dc
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-11-12 14:03:32 +0530

    Testing CI

--- src/main/java/org/lokeshdarla/Main.java
+++ src/main/java/org/lokeshdarla/Main.java
@@ -2,6 +2,6 @@ package org.lokeshdarla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Jenkins!");
     }
 }
