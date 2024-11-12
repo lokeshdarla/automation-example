5f2dbd30a6c9bb6b6751492538f59efe3333170b
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-11-12 14:07:44 +0530

    test commit

--- src/main/java/org/lokeshdarla/Main.java
+++ src/main/java/org/lokeshdarla/Main.java
@@ -2,6 +2,6 @@ package org.lokeshdarla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello World!");
     }
 }
